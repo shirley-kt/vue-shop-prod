80bf1a7b8110ce1d05f6cfdec2d42e7dc6eebb23
raphealshirley <shirleyli8881@gmail.com>
2020-06-26 14:39:25 -0400

    cors

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express')
 const compression = require('compression')
 
 const app = express()
-
 app.use(compression())
 app.use(express.static('./dist'))
+
+// app.all('*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By", ' 3.2.1')
+//     res.header("Content-Type", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+//     next();
+// })
 
 app.listen(8998, () => {
     console.log("server running at http://127.0.0.1:8998")
